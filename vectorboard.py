86415a69723c4b0b08079fe000d2e0945bbc2e08
Shayan <bagherian_s@metaleng.iust.ac.ir>
2026-08-31 23:47:04 +0330

    Shayan: added def move, first_dir and basically it is working

--- vectorboard.py
+++ vectorboard.py
@@ -20,16 +20,49 @@ def nc(point_x,point_y, list): #neighbor check
             neighbors +="r"
     return neighbors
 
-def rns(point_x,ponit_y,list):
-    neighbor = nc(point_x,ponit_y,list)
-    selector = np.random.randint(0,len(neighbor))
-    return str(neighbor[selector])
+def rns(point_x, point_y, grid):
+    neighbors = nc(point_x, point_y, grid)
+    if not neighbors:
+        return "", point_x, point_y
+    selector = np.random.randint(0, len(neighbors))
+    chosen_dir = neighbors[selector]
+    new_x, new_y = point_x, point_y
+    if chosen_dir == "u":
+        new_x -= 1
+    elif chosen_dir == "d":
+        new_x += 1
+    elif chosen_dir == "l":
+        new_y -= 1
+    elif chosen_dir == "r":
+        new_y += 1
+    return chosen_dir, new_x, new_y
+
+def first_dir(x,y,dir):
+    if dir == "u":
+        dir = "d"
+    elif dir == "d":
+        dir = "u"
+    elif dir == "l":
+        dir = "r"
+    elif dir == "r":
+        dir = "l"
+    ingr[x, y] = dir
+    return ingr
+def move(cxindex, cyindex ,ingr):
+    direction, next_x, next_y = rns(cxindex, cyindex, ingr)
+    ingr[cxindex, cyindex] += direction
+    ingr = first_dir(next_x, next_y,direction)
+    return next_x, next_y, ingr
+
 #ingr = initial_grid
-ingr = np.full((n,n),".", dtype="<U2")
+ingr = np.full((n, n), ".", dtype="<U2")
 
-start = np.random.randint(0,n,1)
-ingr[0,start]= "u" + rns(0,start,ingr)
+yindex = np.random.randint(0, n)
+xindex = 0
 
+ingr[xindex, yindex] = "u"
+xindex, yindex, ingr = move(xindex, yindex ,ingr)
+xindex, yindex, ingr = move(xindex, yindex ,ingr)
+xindex, yindex, ingr = move(xindex, yindex ,ingr)
 
-print(start)
 print(ingr)
